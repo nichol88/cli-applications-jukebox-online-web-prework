93a206d5abdc94b3e368cc1796ebddc2d81c3095
Brian Nicholls <nichol88@gmail.com>
2019-11-08 23:20:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,7 @@ end
 
 def play(songs_array)
   puts "Please enter a song name or number:"
-  response = gets.strip
+  response = gets.chomp
 
   songs_array.each_with_index{ |song,index|
     if (index + 1).to_s == response.to_s || response == song
